3ceb6700d365739cd1cc323711ca561005098a8c
Guillermotroya4 <150377903+Guillermotroya4@users.noreply.github.com>
2023-12-11 19:55:18 +0100

    Add files via upload

---
+++ arrays/ejercicio7_arrays.java
@@ -0,0 +1,24 @@
+/**
+ * En este preograma haremos una secuencia de números en la cual se imprimira cada numero las veces que sea el número
+ * @author Guillermo Troya Albarrán
+ */
+public class ejercicio7_arrays{
+public static void main(String[] args) {
+            int tamano = 10;
+            int[] array = new int[tamano * (tamano + 1) / 2]; // Aquí lo que vamos a hacer es calcular el tamaño del array diciendole que tiene que llegar hasta el 10
+    
+            // Aquí vamos a meter los datos en el array
+            int index = 0;
+            for (int i = 1; i <= tamano; i++) {
+                for (int j = 1; j <= i; j++) {
+                    array[index++] = i;
+                }
+            }
+    
+            // Aquí vamos a mostrar el array
+            System.out.println("Secuencia generada:");
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + ",");
+            }
+        }
+}
